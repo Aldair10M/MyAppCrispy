7ac8cb7e431d10f3d0005abd6cdef6f6db7ded6e
Edu Aldair Peña Garcia <edualdairpenagarcia92@gmail.com>
2025-10-26 19:42:26 -0500

    Interfaz de reset-passowrd terminada

--- src/app/features/auth/reset-password/reset-password.page.ts
+++ src/app/features/auth/reset-password/reset-password.page.ts
@@ -1,20 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonItem, IonInput, IonButton, IonImg, IonText } from '@ionic/angular/standalone';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-reset-password',
   templateUrl: './reset-password.page.html',
   styleUrls: ['./reset-password.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonItem, IonInput, IonButton, IonImg, IonText, CommonModule, FormsModule]
 })
 export class ResetPasswordPage implements OnInit {
 
-  constructor() { }
+  password = '';
+  confirmPassword = '';
 
-  ngOnInit() {
+  constructor(private router: Router) {}
+
+  ngOnInit() {}
+
+  resetPassword() {
+    if (!this.password || !this.confirmPassword) {
+      alert('Por favor, completa todos los campos.');
+      return;
+    }
+
+    if (this.password !== this.confirmPassword) {
+      alert('Las contraseñas no coinciden.');
+      return;
+    }
+
+    alert('✅ Contraseña restablecida correctamente.');
+    this.router.navigateByUrl('/login');
   }
-
 }
