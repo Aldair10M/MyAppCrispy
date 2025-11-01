18dc6f80b12faedfc4560246f9812fca0e8922be
Edu Aldair Peña Garcia <edualdairpenagarcia92@gmail.com>
2025-11-01 15:05:35 -0500

    chore: clean HomePage imports and fix build warnings

--- src/app/features/home/home.page.ts
+++ src/app/features/home/home.page.ts
@@ -1,20 +1,56 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {
+  IonContent,
+  IonHeader,
+  IonToolbar,
+  IonIcon,
+  IonChip,
+  IonLabel,
+  IonFooter
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+  IonContent,
+  IonHeader,
+  IonToolbar,
+  IonIcon,
+  IonChip,
+  IonLabel,
+  IonFooter,
+  CommonModule,
+  FormsModule
+  ]
 })
 export class HomePage implements OnInit {
 
+  searchTerm: string = '';
+  categories: string[] = ['Burgers', 'Pizza', 'Sushi', 'Drinks', 'Desserts'];
+  selectedCategory: string | null = null;
+
+  menuItems: Array<{ name: string; image: string; category?: string }> = [
+    { name: 'Classic Burger', image: '/assets/img/burger.jpg', category: 'Burgers' },
+    { name: 'Margherita Pizza', image: '/assets/img/pizza.jpg', category: 'Pizza' },
+    { name: 'California Roll', image: '/assets/img/sushi.jpg', category: 'Sushi' },
+    { name: 'Coke', image: '/assets/img/drinks.jpg', category: 'Drinks' }
+  ];
+
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  selectCategory(category: string) {
+    if (this.selectedCategory === category) {
+      this.selectedCategory = null;
+    } else {
+      this.selectedCategory = category;
+    }
   }
 
 }
