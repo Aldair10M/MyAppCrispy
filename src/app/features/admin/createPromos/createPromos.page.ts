e409e63d2d3f9616c6d89c671ce67582c75be044
Edu Aldair Peña Garcia <edualdairpenagarcia92@gmail.com>
2025-11-15 13:24:43 -0500

    feat(admin): createPromos header style + product listing (select products for promo)

--- src/app/features/admin/createPromos/createPromos.page.ts
+++ src/app/features/admin/createPromos/createPromos.page.ts
@@ -1,16 +1,43 @@
-import { Component } from '@angular/core';
-import { IonContent, IonHeader, IonToolbar, IonInput, IonButton, IonItem, IonLabel, IonTextarea } from '@ionic/angular/standalone';
+import { Component, OnInit } from '@angular/core';
+import { IonContent, IonHeader, IonToolbar, IonInput, IonButton, IonItem, IonLabel, IonTextarea, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonImg } from '@ionic/angular/standalone';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { ProductService } from '../../../core/services/product.service';
+import { Product } from '../../../core/models/product.model';
 
 @Component({
   selector: 'app-create-promos',
   templateUrl: './createPromos.page.html',
   styleUrls: ['./createPromos.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonToolbar, IonInput, IonButton, IonItem, IonLabel, IonTextarea, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonToolbar, IonInput, IonButton, IonItem, IonLabel, IonTextarea, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonImg, CommonModule, FormsModule]
 })
-export class CreatePromosPage {
+export class CreatePromosPage implements OnInit {
   promo = { title: '', description: '', discount: 0 };
-  save() { console.log('Crear promoción', this.promo); }
+  products: Product[] = [];
+  selected: Record<string, boolean> = {};
+
+  constructor(private productService: ProductService) {}
+
+  ngOnInit() {
+    this.loadProducts();
+  }
+
+  loadProducts() {
+    this.productService.getAll().subscribe({
+      next: (items: Product[]) => this.products = items || [],
+      error: (err: any) => console.error('Error loading products for promos', err)
+    });
+  }
+
+  toggleSelect(p: Product) {
+    const id = p.id || p.name;
+    this.selected[id] = !this.selected[id];
+  }
+
+  save() {
+    const selectedIds = this.products.filter(p => this.selected[p.id || p.name]).map(p => p.id || p.name);
+    console.log('Crear promoción', this.promo, 'products:', selectedIds);
+    // TODO: call backend to create promo with products
+  }
 }
